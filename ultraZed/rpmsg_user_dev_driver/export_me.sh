ddca6c4cfceb8192e337a9a9716412526bf4e27d
Adrian Remonda <adrianremonda@gmail.com>
2017-06-23 18:25:34 +0200

    fix makefile

--- ultraZed/rpmsg_user_dev_driver/export_me.sh
+++ ultraZed/rpmsg_user_dev_driver/export_me.sh
@@ -1,4 +1,8 @@
-export ARCH=arm64
-export CC=/opt/Xilinx/SDK/2017.1/gnu/aarch64/lin/aarch64-linux/bin/aarch64-linux-gnu- 
-export CROSS_COMPILE=${CC} 
-#export CROSS32CC=arm-linux-gnueabihf-gcc
+# Use this for the microzed
+#. ../../export_me_uZed.sh
+
+
+
+# Use this for the ultrascale
+MPWD=$(eval pwd)
+cd ../..; 	. ./export_me_ultraZed.sh; cd $MPWD
